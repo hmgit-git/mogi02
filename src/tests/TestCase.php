20a6aa5dca9b3de2946f33e38a92d492c7df9939
miyokey <hamagamimiyoko@gmail.com>
2025-09-28 20:59:14 +0900

    test: fix CSRF in TestCase; docs: testing instructions

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Http\Middleware\VerifyCsrfToken;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // 419（CSRF mismatch）対策：テスト中は CSRF を無効化
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
 }
